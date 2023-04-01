22c10ac9eefcf58aa4687ee716c80a1aa5506a29
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2023-04-01 12:42:23 -0300

    contract for dynamic UI's

---
+++ Spectre.Console.Next/Base/IDynamicUI.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectre.Console.Next.Base;
+
+public interface IDynamicUI
+{
+    bool IsDirty { get; }
+    IRenderable Build();
+}
